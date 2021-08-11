b082a6d63de26eed4f4b648f6c109a512188d3a3
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-08-11 11:37:35 -0700

    Add files via upload

---
+++ i3/bin/cpu_info.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+TEMP=$(sensors | grep 'Package id 0:\|Tdie' | grep ':[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
+CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
+echo "$CPU_USAGE $TEMP" | awk '{ printf("💾 CPU:%6s% @ %s \n"), $1, $2 }'
---
+++ i3/bin/lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
+sleep .1
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert ~/.config/i3/bin/lock.png -resize 400x200 ~/.config/i3/bin/lock.png
+composite -gravity center ~/.config/i3/bin/lock.png /tmp/screen.png /tmp/screen.png 
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
+
